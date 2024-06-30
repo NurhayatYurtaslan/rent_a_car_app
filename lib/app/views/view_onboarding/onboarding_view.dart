4bbb53d09c300324ccff145469adc45d9c42331c
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-06-30 23:55:47 +0300

    update 🆕: car value

--- lib/app/views/view_onboarding/onboarding_view.dart
+++ lib/app/views/view_onboarding/onboarding_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:rent_a_car_app/core/widgets/elevated_button_widget.dart';
+import 'package:rent_a_car_app/data/car_data.dart';
 
 class OnboardingView extends StatelessWidget {
-  const OnboardingView({super.key});
-
+  
+  final Car car;
+const OnboardingView({super.key, required this.car});
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -21,27 +23,29 @@ class OnboardingView extends StatelessWidget {
                 ),
               ),
             ),
-            const Expanded(
+            Expanded(
               child: Column(
                 children: [
-                  Text(
+                  const Text(
                     "Premium cars  \nEnjoy the luxury",
                     style: TextStyle(
                         color: Colors.black54,
                         fontSize: 32,
                         fontWeight: FontWeight.bold),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 10,
                   ),
-                  Text(
+                  const Text(
                     "Premium and prestige car daily rental.  \nExperience the thrill at a lower price",
                     style: TextStyle(color: Colors.grey, fontSize: 16),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 10,
                   ),
-                  ElevatedButtonWidget(),
+                  ElevatedButtonWidget(
+                    car: car
+                  ),
                 ],
               ),
             )
