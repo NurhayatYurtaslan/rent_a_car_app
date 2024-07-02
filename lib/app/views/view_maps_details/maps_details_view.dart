c0309d35a8c3c1143a9b1eec1ab00efe7813ec83
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-07-02 19:26:06 +0300

    rename 🔁: detail to details

---
+++ lib/app/views/view_maps_details/maps_details_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MapsDetailsView extends StatelessWidget {
+  const MapsDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
