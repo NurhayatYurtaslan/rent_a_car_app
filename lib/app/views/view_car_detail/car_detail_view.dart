aa7d8a3bc42886426c415295b351a3683a896722
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-07-06 15:38:12 +0300

    add ✨: animation

--- lib/app/views/view_car_detail/car_detail_view.dart
+++ lib/app/views/view_car_detail/car_detail_view.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:rent_a_car_app/app/views/view_maps_details/maps_details_view.dart';
 import 'package:rent_a_car_app/core/widgets/avatar_widget.dart';
 import 'package:rent_a_car_app/core/widgets/car_card_widget.dart';
-import 'package:rent_a_car_app/core/widgets/maps_widget.dart';
 import 'package:rent_a_car_app/core/widgets/more_car_list_widget.dart';
 import 'package:rent_a_car_app/data/car_data.dart';
 
-class CarDetailView extends StatelessWidget {
+class CarDetailView extends StatefulWidget {
   final Car car;
 
   const CarDetailView({super.key, required this.car});
+
+  @override
+  State<CarDetailView> createState() => _CarDetailViewState();
+}
+
+class _CarDetailViewState extends State<CarDetailView>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _controller;
+  Animation<double>? _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(duration: const Duration(seconds: 3), vsync: this);
+    _animation = Tween<double>(begin: 1.0, end: 1.5).animate(_controller!)
+      ..addListener(() {
+        setState(() {});
+      });
+    _controller!.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller!.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,10 +59,10 @@ class CarDetailView extends StatelessWidget {
         children: [
           CarCardWidget(
             car: Car(
-              model: car.model,
-              distance: car.distance,
-              fuelCapacity: car.fuelCapacity,
-              pricePerHour: car.pricePerHour,
+              model: widget.car.model,
+              distance: widget.car.distance,
+              fuelCapacity: widget.car.fuelCapacity,
+              pricePerHour: widget.car.pricePerHour,
             ),
           ),
           const SizedBox(
@@ -50,15 +77,43 @@ class CarDetailView extends StatelessWidget {
                   width: 20,
                 ),
                 Expanded(
-                  child: MapsWidget(
-                    car: Car(
-                      model: 'Fortuner GR',
-                      distance: 970,
-                      fuelCapacity: 50,
-                      pricePerHour: 45,
+                    child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => MapsDetailsView(
+                          car: Car(
+                            model: 'Fortuner GR',
+                            distance: 970,
+                            fuelCapacity: 50,
+                            pricePerHour: 45,
+                          ), // Use the provided 'car' instance
+                        ),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    height: 170,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 10,
+                          spreadRadius: 5,
+                        ),
+                      ],
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Transform.scale(
+                        scale: _animation!.value,
+                        alignment: Alignment.center,
+                        child: Image.asset('assets/images/maps.png', fit: BoxFit.cover),
+                      ),
                     ),
                   ),
-                ),
+                )),
               ],
             ),
           ),
@@ -69,15 +124,15 @@ class CarDetailView extends StatelessWidget {
             padding: const EdgeInsets.all(20),
             child: Column(
               children: [
-                MoreCarListWidget(car: car),
+                MoreCarListWidget(car: widget.car),
                 const SizedBox(
                   height: 5,
                 ),
-                MoreCarListWidget(car: car),
+                MoreCarListWidget(car: widget.car),
                 const SizedBox(
                   height: 5,
                 ),
-                MoreCarListWidget(car: car),
+                MoreCarListWidget(car: widget.car),
               ],
             ),
           ),
