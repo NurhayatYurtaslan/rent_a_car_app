6b76460a7af5852c63842cdb5a9db51a4e33d06d
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-07-09 20:48:56 +0300

    update 🆕: images

--- lib/app/views/view_onboarding/onboarding_view.dart
+++ lib/app/views/view_onboarding/onboarding_view.dart
@@ -8,49 +8,47 @@ class OnboardingView extends StatelessWidget {
 const OnboardingView({super.key});
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(),
-        body: Column(
-          children: [
-            Expanded(
-              flex: 2,
-              child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage(Assets.images.onboarding.path),
-                      fit: BoxFit.cover),
-                ),
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(Assets.images.onboarding.path),
+                    fit: BoxFit.cover),
               ),
             ),
-            const Expanded(
-              child: Column(
-                children: [
-                  Text(
-                    "Premium cars  \nEnjoy the luxury",
-                    style: TextStyle(
-                        color: Colors.black54,
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "Premium and prestige car daily rental.  \nExperience the thrill at a lower price",
-                    style: TextStyle(color: Colors.grey, fontSize: 16),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  ElevatedButtonWidget(
-                   
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
+          ),
+          const Expanded(
+            child: Column(
+              children: [
+                Text(
+                  "Premium cars  \nEnjoy the luxury",
+                  style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Premium and prestige car daily rental.  \nExperience the thrill at a lower price",
+                  style: TextStyle(color: Colors.grey, fontSize: 16),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                ElevatedButtonWidget(
+                 
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
